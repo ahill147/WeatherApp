5419f0b3cca9d154fe2a2c0f1907faabdd938c11
ahill147 <drewh4511@gmail.com>
2023-10-08 23:20:15 -0500

    Made search bar to see weather in other cities

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '8f4e7d3226msh14342289210f783p1d4f35jsn852413094084'
+};
+
+
+
+
+
